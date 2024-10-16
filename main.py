b0949f7d5973b7768bee8047686edad0965e63e4
asya <asyasusen@gmail.com>
2024-10-17 00:55:59 +0300

    cleanup

--- main.py
+++ main.py
@@ -7,12 +7,3 @@ from society.society_generation.create_random_society import create_random_socie
 from blob.relationship.relationship_manager import Relationship_Manager
 from time_manager.time_manager import Time_Manager
 
-x= create_random_blob()
-y= create_random_blob()
-Relationship_Manager.blobs_meet(x,y)
-rel1 = Relationship_Manager.get_relationship(x,y)
-print(rel1.get_friend_score())
-Time_Manager.pass_days(30)
-print(rel1.get_friend_score())
-Time_Manager.pass_days(30)
-print(rel1.get_friend_score())
--- time_manager/time_manager.py
+++ time_manager/time_manager.py
@@ -14,12 +14,3 @@ class Time_Manager:
     @classmethod
     def get_time(cls):
         return cls.day_count
-    ####TODO DELETE THIS, ONLY FOR REFERANCE
-    def one_year_passes(my_society):
-        next_year_event= Event("Time passes","No matter what we do, we cannot slow down the passage of time. ")
-        next_year_choice = Choice("let the year end")
-        next_year_event.add_choice(next_year_choice)
-        for x in my_society.members:
-            age_up_consq = Consequence(x, "age", 1, f"{x.name} ages." )
-            next_year_choice.add_consequence(age_up_consq)
-        next_year_event.trigger()
